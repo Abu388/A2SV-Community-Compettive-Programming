218ea877cd3e1828ceb8368b2617b37b0fd19a73
Abu388 <150917473+Abu388@users.noreply.github.com>
2024-11-05 09:19:13 +0300

    Time: 0 ms (100.00%), Space: 16.8 MB (10.51%) - LeetHub

--- 0722-remove-comments/0722-remove-comments.py
+++ 0722-remove-comments/0722-remove-comments.py
@@ -5,7 +5,7 @@ class Solution:
 
         for line in source:
             if not in_block_comment:
-                res = []  # Start a new line if not inside a block comment
+                res = []  
 
             i = 0
             while i < len(line):
@@ -13,19 +13,19 @@ class Solution:
                     
                     break
                 elif not in_block_comment and i + 1 < len(line) and line[i] == '/' and line[i + 1] == '*':
-                    # Start of a block comment
+                    
                     in_block_comment = True
-                    i += 1  # Skip next character ('*')
+                    i += 1  
                 elif in_block_comment and i + 1 < len(line) and line[i] == '*' and line[i + 1] == '/':
-                    # End of a block comment
+                    
                     in_block_comment = False
-                    i += 1  # Skip next character ('/')
+                    i += 1  
                 elif not in_block_comment:
-                    # Normal character outside of any comment
+                    
                     res.append(line[i])
                 i += 1
 
-            # If not in a block comment and line has content, add to result
+            
             if res and not in_block_comment:
                 arr.append("".join(res))
 
