c94b0817b2a911d7c9511f13243ba3ad55eafd33
Abu388 <150917473+Abu388@users.noreply.github.com>
2024-11-05 23:09:12 +0300

    Time: 0 ms (100.00%), Space: 16.5 MB (77.56%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.py
@@ -0,0 +1,21 @@
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        
+        subdomains = path.split('/')
+
+        resolved_path = []
+        for part in subdomains:
+            if part == "..":
+                if resolved_path:
+                    resolved_path.pop()  # Go back one directory
+            elif part and part != ".":  # Ignore empty parts and current directory symbol
+                resolved_path.append(part)
+        if not resolved_path:
+            resolved_path.append("/")
+            res = "/".join(resolved_path)
+            return res
+        
+
+        res = "/"+"/".join(resolved_path)
+        
+        return res
