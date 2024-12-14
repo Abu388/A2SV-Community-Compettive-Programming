aa74b514eebe2219dab00ffbc0814345568b5d53
Abu388 <150917473+Abu388@users.noreply.github.com>
2024-12-14 23:27:59 +0300

    Time: 51 ms (6.66%), Space: 21.5 MB (6.10%) - LeetHub

---
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -0,0 +1,14 @@
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        mp = defaultdict(int)
+        rsum , n= 0, len(nums)
+        mp[0] = 1
+        ans = 0
+        for i in range(n):
+            rsum+=nums[i]
+            if mp[rsum-k]>0:
+                ans+=mp[rsum-k]
+            mp[rsum]+=1
+        
+        return ans
+       
