46d6fb230fa075f6ff906f18e55a7e09bfc2ab3e
Abu388 <150917473+Abu388@users.noreply.github.com>
2025-02-07 18:48:46 +0300

    Created 'Presents' by A2SV Hub

---
+++ 07-Feb-2025/Presents 250269.py
@@ -0,0 +1,20 @@
+# Problem: Presents - https://codeforces.com/problemset/problem/136/A
+
+def solution(n,arr):
+    has={}
+    res=[]
+    ind=1
+    for i in arr:
+        has[i]=ind
+        ind+=1
+    arr.sort()
+    
+    for j in arr:
+        res.append(has[j])
+
+    return res
+
+
+n= int(input())
+arr=list(map(int,input().split()))
+print(*solution(n,arr))
