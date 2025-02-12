931ef144594cd968b1f68d3cb6af0fee2386e27d
Abu388 <150917473+Abu388@users.noreply.github.com>
2025-02-12 11:30:48 +0300

    Created 'Number of smaller' by A2SV Hub

---
+++ 12-Feb-2025/Number of smaller 259104.py
@@ -0,0 +1,13 @@
+# Problem: Number of smaller - https://codeforces.com/edu/course/2/lesson/9/1/practice/contest/307092/problem/B
+
+n,m=map(int,input().split())
+arr=list(map(int,input().split()))
+comp=list(map(int,input().split()))
+ind=0
+res=[]
+for i in comp:
+    
+    while ind <len(arr) and i>arr[ind] :
+        ind+=1
+    res.append(ind)
+print(*res)
