e17a6959d9010ab0acafa2c394435a100f22a4d0
Abu388 <150917473+Abu388@users.noreply.github.com>
2025-02-20 23:31:51 +0300

    Created 'B - Special Matrix Quest' by A2SV Hub

---
+++ A2SV G6 - Round #2 20-Feb-2025/B - Special Matrix Quest 277592.py
@@ -0,0 +1,12 @@
+# Problem: B - Special Matrix Quest - https://codeforces.com/gym/586960/problem/B
+
+n=int(input())
+arr=[list(map(int,input().split())) for _ in range(n)]
+res=0
+for row in range(n):
+    res+=arr[row][row]
+    res+=arr[row][n//2]
+    res+=arr[n//2][row]
+    res+=arr[row][n-1-row]
+res-=arr[n//2][n//2]*3
+print(res)
