eeb3e57f0a82d07a20f0ed91f331211fa2ed9626
Abu388 <150917473+Abu388@users.noreply.github.com>
2025-02-21 18:26:30 +0300

    Created 'Karen and Coffee' by A2SV Hub

---
+++ 21-Feb-2025/Karen and Coffee 278874.py
@@ -0,0 +1,35 @@
+# Problem: Karen and Coffee - https://codeforces.com/contest/816/problem/B
+
+n,k,r=map(int,input().split())
+c=[0]*200004
+recipes= [list(map(int,input().split())) for _ in range(n)]
+quires = [list(map(int,input().split())) for _ in range(r)]
+
+
+for start,end in recipes:
+    c[start]+=1
+    c[end+1]-=1
+
+for i in range(len(c)):
+    c[i]+=c[i-1]
+
+for i in range(len(c)):
+    if c[i]>=k:
+        c[i]=1
+    else:
+        c[i]=0
+
+for i in range(1,len(c)):
+    c[i]+=c[i-1]
+
+for left,right in quires:
+    if left==0:
+        print(c[right])
+    else:
+        print(c[right]-c[left-1])
+
+
+
+
+
+
