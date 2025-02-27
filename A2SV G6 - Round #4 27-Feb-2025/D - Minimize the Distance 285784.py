ecfca3c9d3a8343d829aaf97605651b24724ba6a
Abu388 <150917473+Abu388@users.noreply.github.com>
2025-02-27 11:09:38 +0300

    Created 'D - Minimize the Distance' by A2SV Hub

---
+++ A2SV G6 - Round #4 27-Feb-2025/D - Minimize the Distance 285784.py
@@ -0,0 +1,11 @@
+# Problem: D - Minimize the Distance - https://codeforces.com/gym/590053/problem/D
+
+from collections import Counter, defaultdict
+def arr(): return list(map(int, (x for x in input().split())))
+n=int(input())
+nums=arr()
+nums.sort()
+if n %2 !=0:
+    print(nums[n//2])
+else:
+    print(nums[(n//2)-1])
