8912fbf365116a8782bd1bcb35336f4bda618d5e
Abu388 <150917473+Abu388@users.noreply.github.com>
2025-02-27 11:09:54 +0300

    Created 'A - A+ Student' by A2SV Hub

---
+++ A2SV G6 - Round #4 27-Feb-2025/A - A+ Student 285785.py
@@ -0,0 +1,19 @@
+# Problem: A - A+ Student - https://codeforces.com/gym/590053/problem/A
+
+from collections import Counter, defaultdict
+def arr(): return list(map(int, (x for x in input().split())))
+
+def findA(a,b,c):
+    m=max(0,max(b,c)-a+1)
+    return m 
+def findB(a,b,c):
+    m=max(0,max(a,c)-b+1)
+    return m
+def findC(a,b,c):
+    m=max(0,max(a,b)-c+1)
+    return m
+
+t=int(input())
+for _ in range(t):
+    a,b,c=arr()
+    print(findA(a,b,c),findB(a,b,c),findC(a,b,c))
