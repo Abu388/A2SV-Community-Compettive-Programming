7da4048916eb2dce7e8a2fe30a4f8898e46a7ec5
Abu388 <150917473+Abu388@users.noreply.github.com>
2025-03-03 23:26:47 +0300

    Created 'Red and Blue' by A2SV Hub

---
+++ 03-Mar-2025/Red and Blue 290212.py
@@ -0,0 +1,23 @@
+# Problem: Red and Blue - https://codeforces.com/contest/1469/problem/B
+
+from collections import Counter, defaultdict
+def arr(): return list(map(int, (x for x in input().split())))
+t=int(input())
+for _ in range(t):
+    n=int(input())
+    nums=arr()
+    m=int(input())
+    nums2=arr()
+    pre=[]
+    prefix=[]
+    total=0
+    for i in nums:
+        total+=i
+        pre.append(total)
+    total=0
+    for i in nums2:
+        total+=i
+        prefix.append(total)
+
+    result=max(0,max(pre),max(prefix),max(prefix)+max(pre))
+    print(result)
