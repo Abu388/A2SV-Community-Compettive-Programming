bd4cc731dcd41b8b5ea88189be62df1628c2c8ec
Abu388 <150917473+Abu388@users.noreply.github.com>
2025-03-06 11:57:39 +0300

    Created 'Crawler Log Folder' by A2SV Hub

---
+++ 06-Mar-2025/Crawler Log Folder 294029.py
@@ -0,0 +1,18 @@
+# Problem: Crawler Log Folder - https://leetcode.com/problems/crawler-log-folder/
+
+class Solution:
+    def minOperations(self, logs: List[str]) -> int:
+        count=0
+        for i in range(len(logs)):
+            if logs[i]=="../":
+                if count>0:
+                    count-=1
+                
+            elif logs[i] == './':
+                count=count
+            else:
+                count+=1
+        return count
+            
+       
+       
