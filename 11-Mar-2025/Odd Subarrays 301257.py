967834b0eb3cef020010a6926d9b2b0f5a5e5089
Abu388 <150917473+Abu388@users.noreply.github.com>
2025-03-11 11:56:11 +0300

    Created 'Odd Subarrays' by A2SV Hub

---
+++ 11-Mar-2025/Odd Subarrays 301257.py
@@ -0,0 +1,20 @@
+# Problem: Odd Subarrays - https://codeforces.com/problemset/problem/1686/B
+
+from collections import Counter, defaultdict,deque
+from math import ceil
+def arr(): return list(map(int, (x for x in input().split())))#strip
+def test(): return int(input())
+t=test()
+for _ in range(t):
+    n=test()
+    nums=arr()
+    count=0
+    stk=[]
+    for i in nums:
+        if stk and i <stk[-1]:
+            stk=[]
+            count+=1
+        else:
+            stk.append(i)
+
+    print(count)
