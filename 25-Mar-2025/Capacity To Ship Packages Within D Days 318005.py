4545a9b4ae4a7266000b2657dbc086c7da7c2d4f
Abu388 <150917473+Abu388@users.noreply.github.com>
2025-03-25 19:11:23 +0300

    Created 'Capacity To Ship Packages Within D Days' by A2SV Hub

---
+++ 25-Mar-2025/Capacity To Ship Packages Within D Days 318005.py
@@ -0,0 +1,26 @@
+# Problem: Capacity To Ship Packages Within D Days - https://leetcode.com/problems/capacity-to-ship-packages-within-d-days/
+
+class Solution:
+    def shipWithinDays(self, weights: List[int], days: int) -> int:
+        l,r = max(weights),sum(weights)
+
+        def solu(mid):
+            rsum = 0
+            day = 1
+            for weight in weights:
+                rsum += weight
+
+                if rsum > mid:
+                    day += 1
+                    rsum = weight
+            if day > days:
+                return False
+            return True
+
+        while l<=r:
+            mid = (l+r)//2
+            if solu(mid):
+                r = mid - 1
+            else:
+                l = mid + 1
+        return l
