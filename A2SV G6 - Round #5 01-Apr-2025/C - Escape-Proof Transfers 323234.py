bafcdfeb85448583d5ce8c4170ae4067bc4c03a1
Abu388 <150917473+Abu388@users.noreply.github.com>
2025-04-01 16:35:58 +0300

    Created 'C - Escape-Proof Transfers' by A2SV Hub

---
+++ A2SV G6 - Round #5 01-Apr-2025/C - Escape-Proof Transfers 323234.py
@@ -0,0 +1,19 @@
+# Problem: C - Escape-Proof Transfers - https://codeforces.com/gym/591928/problem/C
+
+from collections import Counter, defaultdict
+def arr(): return list(map(int, (x for x in input().split())))
+
+n,t,c=arr()
+nums=arr()
+count=0
+res=0
+for i in nums:
+    if i<=t:
+        count+=1
+    else:
+        count=0
+    if count>=c:
+        res+=1
+print(res)
+
+
