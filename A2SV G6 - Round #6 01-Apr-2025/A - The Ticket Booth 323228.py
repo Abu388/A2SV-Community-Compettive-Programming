33f6ecce31eb3b032e428fcdfc6b6b1b92446c10
Abu388 <150917473+Abu388@users.noreply.github.com>
2025-04-01 16:34:08 +0300

    Created 'A - The Ticket Booth' by A2SV Hub

---
+++ A2SV G6 - Round #6 01-Apr-2025/A - The Ticket Booth 323228.py
@@ -0,0 +1,18 @@
+# Problem: A - The Ticket Booth - https://codeforces.com/gym/594077/problem/A
+
+from collections import Counter, defaultdict
+from math import ceil
+def arr(): return list(map(int, (x for x in input().split())))
+def test(): return int(input())
+n,s=arr()
+res=0
+
+while s!=0 :
+    if s>=n:
+        res+=1
+        
+        s=s-n
+           
+    else:
+        n-=1
+print(res)
