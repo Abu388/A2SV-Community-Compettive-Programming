f96df143ed50d587fe02475f47210cb91475db6c
Abu388 <150917473+Abu388@users.noreply.github.com>
2025-04-01 16:34:00 +0300

    Created 'D - Meklit's Chat Screen' by A2SV Hub

---
+++ A2SV G6 - Round #6 01-Apr-2025/D - Meklit's Chat Screen 323227.py
@@ -0,0 +1,26 @@
+# Problem: D - Meklit's Chat Screen - https://codeforces.com/gym/594077/problem/D
+
+from collections import Counter, defaultdict,deque
+from math import ceil
+def arr(): return list(map(int, (x for x in input().split())))
+def test(): return int(input())
+
+
+stk=deque()
+n,k=arr()
+nums=arr()
+helper=set()
+
+for i in nums:
+    
+    if i not in helper and len(stk) < k:#o(n^2) chaking so what shall i do?:
+        stk.appendleft(i)
+        helper.add(i)
+    elif i not in helper:
+        stk.appendleft(i)   
+        helper.add(i)
+        temp  = stk.pop()
+        helper.remove(temp)
+        
+print(len(stk))
+print(*stk)
