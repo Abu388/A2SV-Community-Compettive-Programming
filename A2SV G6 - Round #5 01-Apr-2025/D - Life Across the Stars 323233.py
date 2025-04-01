9ec97844b6c02fb05e0bad9f02240d3c003f694c
Abu388 <150917473+Abu388@users.noreply.github.com>
2025-04-01 16:35:56 +0300

    Created 'D - Life Across the Stars' by A2SV Hub

---
+++ A2SV G6 - Round #5 01-Apr-2025/D - Life Across the Stars 323233.py
@@ -0,0 +1,23 @@
+# Problem: D - Life Across the Stars - https://codeforces.com/gym/591928/problem/D
+
+from collections import Counter, defaultdict
+def arr(): return list(map(int, (x for x in input().split())))
+
+mp=defaultdict(int)
+
+t= int(input())
+for _ in range(t):
+    i,j=arr()
+    mp[i]+=1
+    mp[j]-=1
+mp=dict(sorted(mp.items()))
+res=0
+total=0
+for k,v in mp.items():
+    total+=v
+    res=max(res,total)
+    mp[k]=total
+for k , v in mp.items():
+    if v==res:
+        print(k,v)
+        break
