97f9d36766cf93fc0cfc1ae7aeb8d01543e54c34
Abu388 <150917473+Abu388@users.noreply.github.com>
2025-04-01 16:34:19 +0300

    Created 'C - Permutation Minimization' by A2SV Hub

---
+++ A2SV G6 - Round #6 01-Apr-2025/C - Permutation Minimization 323230.py
@@ -0,0 +1,19 @@
+# Problem: C - Permutation Minimization - https://codeforces.com/gym/594077/problem/C
+
+from collections import Counter, defaultdict, deque
+
+from math import ceil
+def arr(): return list(map(int, (x for x in input().split())))
+def test(): return int(input())
+t=test()
+for _ in range(t):
+    
+    n=test()
+    nums=arr()
+    stk=deque()
+    for i in nums:
+        if  stk and stk[0]>i:
+            stk.appendleft(i)
+        else:
+            stk.append(i)
+    print(*stk)
