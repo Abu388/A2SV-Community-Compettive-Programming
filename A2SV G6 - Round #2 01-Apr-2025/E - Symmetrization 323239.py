1513641fbd53e371442531d1142313087dfbfa7d
Abu388 <150917473+Abu388@users.noreply.github.com>
2025-04-01 16:37:50 +0300

    Created 'E - Symmetrization' by A2SV Hub

---
+++ A2SV G6 - Round #2 01-Apr-2025/E - Symmetrization 323239.py
@@ -0,0 +1,24 @@
+# Problem: E - Symmetrization - https://codeforces.com/gym/586960/problem/E
+
+t= int(input())
+for _ in range(t):
+    n = int(input())
+    matrix=[list(map(int,input())) for _ in range(n)]
+    res=0
+    
+    for row in range(n//2):
+        for col in range(row,n-1-row):
+            dec=[(row,col),(col,n-1-row),(n-1-row,n-1-col),(n-1-col,row)]
+
+            count_zero=0
+            count_one=0
+            
+            for a,b in dec:
+                if matrix[a][b]==0:
+                    count_zero+=1
+                else:
+                    count_one+=1
+            res+=min(count_one,count_zero)
+        
+    
+    print(res)
