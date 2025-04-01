44f8985e0dbd7bfcc81843d69c05c6e33f2c5c84
Abu388 <150917473+Abu388@users.noreply.github.com>
2025-04-01 16:37:33 +0300

    Created 'C - The Splitting Game' by A2SV Hub

---
+++ A2SV G6 - Round #2 01-Apr-2025/C - The Splitting Game 323237.py
@@ -0,0 +1,26 @@
+# Problem: C - The Splitting Game - https://codeforces.com/gym/586960/problem/C
+
+from collections import Counter
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    s = input()
+
+    left_set = Counter()
+    right_count = Counter(s)
+    max_sum = 0
+    
+  
+    for i in range(n - 1):
+        left_set[s[i]]+=1      
+        right_count[s[i]] -= 1  
+        
+        if right_count[s[i]] == 0:
+            del right_count[s[i]]  
+        
+       
+        current_sum = len(left_set) + len(right_count)
+        max_sum = max(max_sum, current_sum)
+    
+    print(max_sum)
