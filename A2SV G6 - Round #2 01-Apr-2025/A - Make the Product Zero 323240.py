1efaa5d0d734cad0aca847f241b667187cf2dfb8
Abu388 <150917473+Abu388@users.noreply.github.com>
2025-04-01 16:37:59 +0300

    Created 'A - Make the Product Zero' by A2SV Hub

---
+++ A2SV G6 - Round #2 01-Apr-2025/A - Make the Product Zero 323240.py
@@ -0,0 +1,8 @@
+# Problem: A - Make the Product Zero - https://codeforces.com/gym/586960/problem/A
+
+n=int(input())
+arr=list(map(int,input().split()))
+res=float('inf')
+for i in arr:
+    res=min(abs(i),res)
+print(res)
