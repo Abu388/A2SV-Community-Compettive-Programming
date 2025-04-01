41a45cd4dff484dbd7297885df8c78086c328840
Abu388 <150917473+Abu388@users.noreply.github.com>
2025-04-01 16:34:23 +0300

    Created 'B - Nafargi's Binary Reduction Game' by A2SV Hub

---
+++ A2SV G6 - Round #6 01-Apr-2025/B - Nafargi's Binary Reduction Game 323231.py
@@ -0,0 +1,20 @@
+# Problem: B - Nafargi's Binary Reduction Game - https://codeforces.com/gym/594077/problem/B
+
+from collections import Counter, defaultdict
+from math import ceil
+def arr(): return list(map(int, (x for x in input().strip())))
+def test(): return int(input())
+
+
+t=test()
+nums=arr()
+
+stk=[]
+for i in nums:
+ 
+    if stk and stk[-1]!=i:
+        stk.pop()
+    else:
+        stk.append(i)
+
+print(len(stk))
