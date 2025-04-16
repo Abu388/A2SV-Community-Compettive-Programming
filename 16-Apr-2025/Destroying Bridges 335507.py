9c9410758b4b87fa90cbbfe601085d2a28647de0
Abu388 <150917473+Abu388@users.noreply.github.com>
2025-04-16 20:26:11 +0300

    Created 'Destroying Bridges' by A2SV Hub

---
+++ 16-Apr-2025/Destroying Bridges 335507.py
@@ -0,0 +1,23 @@
+# Problem: Destroying Bridges - https://codeforces.com/problemset/problem/1944/A
+
+import bisect #arr = [1, 2, 4, 4, 5] , index = bisect.bisect_right(arr, 4)
+import sys
+from fractions import Fraction # to use fraction
+import sys, threading
+sys.setrecursionlimit(1 << 30)
+threading.stack_size(1 << 27)
+input = sys.stdin.readline
+from itertools import accumulate # to work on prefix
+from collections import Counter, defaultdict,deque
+from math import ceil,floor
+def arr(): return list(map(int, (x for x in input().split())))#strip
+def test(): return int(input())
+
+
+t = test()
+for _ in range(t):
+    n , k = arr()
+    if k >= n or k == n-1:
+        print(1)
+    else:
+        print(n)
