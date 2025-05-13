2b23c9613b0bd783d4d3c4e83ff364b125c2c17d
Abu388 <150917473+Abu388@users.noreply.github.com>
2025-05-13 11:37:25 +0300

    Created 'People are leaving' by A2SV Hub

---
+++ 13-May-2025/People are leaving 354197.py
@@ -0,0 +1,55 @@
+# Problem: People are leaving - https://codeforces.com/edu/course/2/lesson/7/2/practice/contest/289391/problem/A
+
+import bisect #arr = [1, 2, 4, 4, 5] , index = bisect.bisect_right(arr, 4)
+import sys
+from fractions import Fraction # to use fraction
+import sys, threading
+sys.setrecursionlimit(1 << 30)
+threading.stack_size(1 << 27)
+input = sys.stdin.readline
+from heapq import *
+from itertools import accumulate # to work on prefix
+from collections import Counter, defaultdict,deque
+from math import ceil,floor
+def arr(): return list(map(int, (x for x in input().split())))#strip
+def test(): return int(input())
+
+
+
+
+
+
+n , m = list(map(int,input().split()))
+
+parent =[0] + [i for i in range(1,n+2)]
+
+def find(x):
+    if x > n :
+        return -1
+    if parent[x] != x:
+        parent[x] = find(parent[x]) 
+    return parent[x]
+
+def union(u, v):
+
+
+    xu = find(u)
+    yu = find(v)
+    parent[xu] = parent[yu]
+
+    
+    
+
+
+
+
+
+
+for _ in range(m):
+    val , num = input().split()
+    if val == '?':
+        print(str(find(int(num))))
+    else:
+        union(int(num), int(num)+1)
+
+
