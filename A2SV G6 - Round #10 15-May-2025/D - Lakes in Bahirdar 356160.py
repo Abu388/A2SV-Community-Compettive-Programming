b95bd358e414f20b0fc04c04629fccb106eee4a4
Abu388 <150917473+Abu388@users.noreply.github.com>
2025-05-15 16:09:40 +0300

    Created 'D - Lakes in Bahirdar' by A2SV Hub

---
+++ A2SV G6 - Round #10 15-May-2025/D - Lakes in Bahirdar 356160.py
@@ -0,0 +1,66 @@
+# Problem: D - Lakes in Bahirdar - https://codeforces.com/gym/602812/problem/D
+
+import sys
+import threading
+
+def solve():
+    n, m, k = map(int, input().split())
+    grid = [input() for _ in range(n)]
+
+    def is_border(x, y):
+        return x == 0 or y == 0 or x == n - 1 or y == m - 1
+
+    def in_bounds(x, y):
+        return 0 <= x < n and 0 <= y < m
+
+    def dfs(x, y):
+        vis.add((x, y))
+        nonlocal size, is_lake, lake_cells
+        lake_cells.add((x, y))
+        if is_border(x, y):
+            is_lake = False
+        size += 1
+
+        for dx, dy in dirs:
+            nx, ny = x + dx, y + dy
+            if in_bounds(nx, ny) and (nx, ny) not in vis and grid[nx][ny] == ".":
+                dfs(nx, ny)
+
+    vis = set()
+    lakes = []
+    dirs = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+
+    for i in range(n):
+        for j in range(m):
+            if grid[i][j] == "." and (i, j) not in vis:
+                size = 0
+                is_lake = True
+                lake_cells = set()
+                dfs(i, j)
+                if is_lake:
+                    lakes.append([size, lake_cells])
+
+    lakes.sort()
+    to_fill = len(lakes) - k
+    cnt = 0
+
+    ans = [list(row) for row in grid]
+    for i in range(to_fill):
+        cnt += lakes[i][0]
+        for x, y in lakes[i][1]:
+            ans[x][y] = "*"
+
+    print(cnt)
+    for row in ans:
+        print("".join(row))
+
+input = lambda: sys.stdin.readline().strip()
+
+def main():
+    solve()
+
+if __name__ == '__main__':
+    sys.setrecursionlimit(1 << 30)
+    threading.stack_size(1 << 27)
+    threading.Thread(target=main).start()
+
