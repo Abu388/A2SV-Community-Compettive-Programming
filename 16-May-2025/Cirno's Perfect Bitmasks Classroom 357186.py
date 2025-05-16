1cec0c46d259846d226505322f1b4612306b1758
Abu388 <150917473+Abu388@users.noreply.github.com>
2025-05-16 20:19:03 +0300

    Created 'Cirno's Perfect Bitmasks Classroom' by A2SV Hub

---
+++ 16-May-2025/Cirno's Perfect Bitmasks Classroom 357186.py
@@ -0,0 +1,24 @@
+# Problem: Cirno's Perfect Bitmasks Classroom - https://codeforces.com/problemset/problem/1688/A
+
+
+from math import ceil,floor
+def arr(): return list(map(int, (x for x in input().split())))#strip
+def test(): return int(input())
+
+t = test()
+for _ in range(t):
+    x = test()
+    c = 1
+    for j in range(32):
+        if x & c << j :
+            c = c <<j
+            break     
+    if c ^ x > 0 and c & x > 0:
+        print(c)  
+    else:
+        zero = 1
+        for j in range(32):
+            if not (x & zero << j):
+                zero = zero << j
+                break
+        print(c+zero)
