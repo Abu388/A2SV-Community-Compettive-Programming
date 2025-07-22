e392cef6be55157540e8b4e3b0b65c21186ef0d9
Abu388 <150917473+Abu388@users.noreply.github.com>
2025-07-22 22:25:10 +0300

    Created 'B - The Lord of Medianfell' by A2SV Hub

---
+++ A2SV G6 - Round #8 22-Jul-2025/B - The Lord of Medianfell 378682.py
@@ -0,0 +1,12 @@
+# Problem: B - The Lord of Medianfell - https://codeforces.com/gym/599383/problem/B
+
+
+def arr(): return list(map(int, (x for x in input().split())))#strip
+def test(): return int(input())
+t = test()
+for _ in range(t):
+    n, s = arr()
+    k = (n// 2) + 1
+    print((s // k))
+
+
