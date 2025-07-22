7d92f247ba8a67e2a6b19bfeead8679827fbdc17
Abu388 <150917473+Abu388@users.noreply.github.com>
2025-07-22 23:31:36 +0300

    Created 'C - Arya Meets the Night King ' by A2SV Hub

---
+++ A2SV G6 - Round #8 22-Jul-2025/C - Arya Meets the Night King  378750.py
@@ -0,0 +1,45 @@
+# Problem: C - Arya Meets the Night King  - https://codeforces.com/gym/599383/problem/C
+
+from collections import Counter, defaultdict,deque
+from math import ceil,floor
+def arr(): return list(map(int, (x for x in input().split())))#strip
+def test(): return int(input())
+
+s , b = arr()
+nums = arr()
+res = [0]*len(nums)
+mp = defaultdict(int)
+for l, i in enumerate(nums):
+    mp[l] += i
+smp = dict(sorted(mp.items(), key=lambda x:x[1]))
+
+val = []
+for _ in range(b):
+    a , b = arr()
+    val.append([a,b])
+
+val.sort()
+prifix = 0
+for i in range(len(val)):
+    prifix += val[i][1]
+    val[i][1] = prifix
+
+l = 0
+for k , v in smp.items():
+    
+    if v < val[0][0]:
+        res[k] = 0
+    elif v >= val[len(val)-1][0]:
+        
+        res[k] = val[len(val)-1][1]
+    else:
+        
+        while val[l][0] < v:
+            l += 1
+        if val[l][0] > v:
+            l -= 1
+        res[k] = val[l][1]
+print(*res)
+
+
+
