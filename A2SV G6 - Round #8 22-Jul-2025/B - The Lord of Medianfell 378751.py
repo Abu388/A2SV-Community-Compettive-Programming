e43143d70fddff759a4b6ea857cda050dc5bb719
Abu388 <150917473+Abu388@users.noreply.github.com>
2025-07-22 23:32:08 +0300

    Created 'B - The Lord of Medianfell' by A2SV Hub

---
+++ A2SV G6 - Round #8 22-Jul-2025/B - The Lord of Medianfell 378751.py
@@ -0,0 +1,22 @@
+# Problem: B - The Lord of Medianfell - https://codeforces.com/gym/599383/problem/B
+
+from math import floor
+
+def arr(): 
+    return list(map(int, input().split()))
+
+def test(): 
+    return int(input())
+
+t = test()
+for _ in range(t):
+    n, s = arr()
+    m = (n // 2) + 1  
+    max_median = s // m
+    print(max_median)
+
+
+
+
+
+
