1c54796dc68e9409688d89daa62eafbbf332a447
Abu388 <150917473+Abu388@users.noreply.github.com>
2025-08-14 16:50:31 +0300

    Created 'F - Array Transformation' by A2SV Hub

---
+++ A2SV G6 - Round #2 14-Aug-2025/F - Array Transformation 385018.py
@@ -0,0 +1,37 @@
+# Problem: F - Array Transformation - https://codeforces.com/gym/586960/problem/F
+
+from collections import Counter
+
+def arr(): return list(map(int, input().split()))
+def test(): return int(input())
+
+def solve():
+    t = test()
+    for _ in range(t):
+        n = test()
+        a = arr()
+        
+        freq = Counter(a)
+        
+        freq_counts = Counter(freq.values())
+        
+        unique_freqs = sorted(freq_counts.keys())
+        
+        min_removals = float('inf')
+        
+        for C in unique_freqs:
+            total = 0
+            for k in unique_freqs:
+
+                if k < C:
+                    total += freq_counts[k] * k
+
+                else:
+                    total += freq_counts[k] * (k - C)
+
+            if total < min_removals:
+                min_removals = total
+        
+        print(min_removals)
+
+solve()
