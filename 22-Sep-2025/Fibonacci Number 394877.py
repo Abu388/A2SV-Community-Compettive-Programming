9500b94cc48ae69c78f2d7ea3e5f8ce6c3ea2063
Abenezer Mesfin <150917473+Abu388@users.noreply.github.com>
2025-09-22 11:01:29 +0300

    Created 'Fibonacci Number' by A2SV Hub

---
+++ 22-Sep-2025/Fibonacci Number 394877.py
@@ -0,0 +1,20 @@
+# Problem: Fibonacci Number - https://leetcode.com/problems/fibonacci-number/
+
+class Solution:
+    def fib(self, n: int) -> int:
+        mp = defaultdict(int)
+       
+        def solu(n):
+            if n == 1:
+                return 1
+            if n == 0:
+                return 0
+            if n not in mp:
+                mp[n] = solu( n - 1) + solu(n - 2)
+            return mp[n]
+        return solu(n)
+            
+
+
+        
+    
