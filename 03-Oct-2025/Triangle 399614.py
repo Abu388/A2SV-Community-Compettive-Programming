806bce93383f1fb9f7d6b5345381985d9d5d3b34
Abenezer Mesfin <150917473+Abu388@users.noreply.github.com>
2025-10-03 23:41:43 +0300

    Created 'Triangle' by A2SV Hub

---
+++ 03-Oct-2025/Triangle 399614.py
@@ -0,0 +1,15 @@
+# Problem: Triangle - https://leetcode.com/problems/triangle/
+
+class Solution:
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        @cache
+        def dp(i,j ):
+            
+            if i >= len(triangle ):
+                return 0
+            
+                    
+            return triangle[i][j] + min(dp(i + 1, j) , dp(i + 1, j + 1))
+
+       
+        return dp(0,0)
