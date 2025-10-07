212570f283b8ecc2c9e33e9645c045939fa0dc2e
Abenezer Mesfin <150917473+Abu388@users.noreply.github.com>
2025-10-07 19:30:02 +0300

    Created 'Almost Prime' by A2SV Hub

---
+++ 07-Oct-2025/Almost Prime 400599.py
@@ -0,0 +1,35 @@
+# Problem: Almost Prime - https://codeforces.com/problemset/problem/26/A
+
+def arr(): return list(map(int, (x for x in input().split())))#strip
+def test(): return int(input())
+
+
+
+
+def trial_division_simple(n) :
+    factorization = set()
+    d = 2
+    while d * d <= n:
+
+        while n % d == 0:
+            
+            factorization.add(d)
+            
+            n //= d
+
+        d += 1
+    if n > 1:
+        factorization.add(n)
+        
+    
+    return factorization
+
+
+
+t = test()
+c = 0
+for i in range(4, t + 1):
+    res = trial_division_simple(i)
+    if len(res) == 2:
+        c += 1
+print(c)
