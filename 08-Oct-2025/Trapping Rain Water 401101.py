6f061427437299bce62fa19bf1ec3f308cfdfd2d
Abenezer Mesfin <150917473+Abu388@users.noreply.github.com>
2025-10-08 20:05:55 +0300

    Created 'Trapping Rain Water' by A2SV Hub

---
+++ 08-Oct-2025/Trapping Rain Water 401101.py
@@ -0,0 +1,33 @@
+# Problem: Trapping Rain Water - https://leetcode.com/problems/trapping-rain-water/
+
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        stk = []
+        res = 0
+        for i in range(len(height)):
+            if not stk:
+                stk.append((i, height[i]))
+            else:
+                
+                index , temp = stk[-1]
+                
+
+                while stk and len(stk) > 1 and temp <=  height[i]:
+                
+                    newind , newtemp = stk.pop()
+                    index , temp = stk[-1]
+                    res += ( min(temp , height[i]) - newtemp ) * (i - index - 1)
+                while stk and stk[-1][1] < height[i]:
+                    stk.pop()
+                stk.append((i, height[i]))
+
+        
+        return res
+
+                    
+
+
+                        
+
+
+        
