353c58770ef3e46c4ebfc06ae92e6cdc009a8ac5
Abenezer Mesfin <150917473+Abu388@users.noreply.github.com>
2025-10-23 11:07:48 +0300

    Created 'Candy' by A2SV Hub

---
+++ 23-Oct-2025/Candy 405205.py
@@ -0,0 +1,15 @@
+# Problem: Candy - https://leetcode.com/problems/candy/
+
+class Solution:
+    def candy(self, ratings: List[int]) -> int:
+        res = [1] * len(ratings)
+        for i in range( len(ratings) - 1):
+            if ratings[i] < ratings[i + 1]:
+                res[i + 1] = res[i] + 1
+        
+        for i in range(len(ratings) - 2 , -1 , - 1):
+            if ratings[i] > ratings[i + 1]:
+                if  res[i] <= res[i + 1]:
+                    res[i] = max(res[i], res[i + 1] + 1)
+        return sum(res)
+
