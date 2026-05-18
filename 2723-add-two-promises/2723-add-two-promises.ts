776bf3727fed7c1d6662f47518148cefaff605c2
Abenezer Mesfin <150917473+Abu388@users.noreply.github.com>
2026-05-18 21:55:48 +0300

    Time: 53 ms (64.74%), Space: 54.9 MB (81.93%) - LeetHub

--- 2723-add-two-promises/2723-add-two-promises.ts
+++ 2723-add-two-promises/2723-add-two-promises.ts
@@ -2,9 +2,8 @@ type P = Promise<number>
 
 async function addTwoPromises(promise1: P, promise2: P): P {
     console.log(promise1)
-    const p1 = await promise1 
-    const p2 = await promise2
-    return p1 + p2
+    
+    return await promise1  + await promise2
     // return promise1 + promise2
 };
 
