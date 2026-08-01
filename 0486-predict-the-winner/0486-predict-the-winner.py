49c8677dcc0daedd5d55f1bae70bc9d450a87774
Abenezer Mesfin <150917473+Abu388@users.noreply.github.com>
2026-08-01 13:33:34 +0300

    Time: 1621 ms (14.92%), Space: 19.3 MB (79.49%) - LeetHub

---
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -0,0 +1,9 @@
+class Solution:
+    def predictTheWinner(self, nums: List[int]) -> bool:
+        def solu(left,right):
+            if left == right:
+                return nums[right]
+            pl = nums[left] - solu(left+1,right)
+            pr = nums[right] - solu(left,right-1)
+            return max(pl,pr)
+        return solu(0,len(nums)-1) >= 0
